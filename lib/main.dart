9d2426fc0926d7f26d6ea023f1c1e45fa1a51cb6
seankeish111 <shankesh.s@outlook.com>
2022-09-22 20:53:56 +1000

    UI implemented with aws

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,16 @@
+// dart async library we will refer to when setting up real time updates
+import 'dart:async';
+
+// flutter and ui libraries
 import 'package:flutter/material.dart';
+
+// amplify packages we will need to use
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:amplify_datastore/amplify_datastore.dart';
+
+// amplify configuration and models that should have been generated for you
+import 'amplifyconfiguration.dart';
+import 'models/ModelProvider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,109 +19,190 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      title: 'Enquiries',
+      home: EnquiriesPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class EnquiriesPage extends StatefulWidget {
+  const EnquiriesPage({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<EnquiriesPage> createState() => _EnquiriesPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _EnquiriesPageState extends State<EnquiriesPage> {
+  @override
+  void initState() {
+    // to be filled in a later step
+    super.initState();
+  }
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void dispose() {
+    // to be filled in a later step
+    super.dispose();
+  }
+
+  Future<void> _initializeApp() async {
+    // to be filled in a later step
+  }
+
+  Future<void> _configureAmplify() async {
+    // to be filled in a later step
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Enquiries'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: const Center(child: CircularProgressIndicator()),
+      // body: _isLoading
+      //     ? Center(child: CircularProgressIndicator())
+      //     : TodosList(todos: _todos),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const AddEnquiryForm()),
+          );
+        },
+        tooltip: 'Enquire',
+        label: Row(
+          children: const [Icon(Icons.add), Text('Add Enquiry')],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
+
+class EnquiriesList extends StatelessWidget {
+  const EnquiriesList({
+    required this.enquiries,
+    Key? key,
+  }) : super(key: key);
+
+  final List<Enquiry> enquiries;
+
+  @override
+  Widget build(BuildContext context) {
+    return enquiries.isNotEmpty
+        ? ListView(
+        padding: const EdgeInsets.all(8),
+        children: enquiries.map((enquiry) => EnquiryItem(enquiry: enquiry)).toList())
+        : const Center(
+      child: Text('Tap button below to add a todo!'),
+    );
+  }
+}
+
+class EnquiryItem extends StatelessWidget {
+  const EnquiryItem({
+    required this.enquiry,
+    Key? key,
+  }) : super(key: key);
+
+  final double iconSize = 24.0;
+  final Enquiry enquiry;
+
+  void _deleteEnquiry (BuildContext context) async {
+    // to be filled in a later step
+  }
+
+  Future<void> _toggleIsComplete() async {
+    // to be filled in a later step
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: InkWell(
+        onTap: () {
+          _toggleIsComplete();
+        },
+        onLongPress: () {
+          _deleteEnquiry(context);
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    enquiry.name,
+                    style: const TextStyle(
+                        fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  Text(enquiry.description ?? 'No description'),
+                ],
+              ),
+            ),
+            Icon(
+                enquiry.isComplete
+                    ? Icons.check_box
+                    : Icons.check_box_outline_blank,
+                size: iconSize),
+          ]),
+        ),
+      ),
+    );
+  }
+}
+
+class AddEnquiryForm extends StatefulWidget {
+  const AddEnquiryForm({Key? key}) : super(key: key);
+
+  @override
+  State<AddEnquiryForm> createState() => _AddEnquiryFormState();
+}
+
+class _AddEnquiryFormState extends State<AddEnquiryForm> {
+  final _nameController = TextEditingController();
+  final _descriptionController = TextEditingController();
+
+  Future<void> _saveEnquiry() async {
+    // to be filled in a later step
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add Enquiry'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextFormField(
+                controller: _nameController,
+                decoration:
+                const InputDecoration(filled: true, labelText: 'Name'),
+              ),
+              TextFormField(
+                controller: _descriptionController,
+                decoration: const InputDecoration(
+                    filled: true, labelText: 'Description'),
+              ),
+              ElevatedButton(
+                onPressed: _saveEnquiry,
+                child: const Text('Save'),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
